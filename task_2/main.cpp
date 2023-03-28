f2acbd086ba08aa8a3235b5bc56eed1ee4f100bb
styushin <84208644+styushin@users.noreply.github.com>
2023-03-29 00:04:56 +0300

    Update main.cpp

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -3,41 +3,42 @@
 
 int main ()
 {
-    unsigned op, alg;
+    unsigned op, alg; //неопределенные переменные
     string file_in, file_out, key;
     do {
-        cout<<"Select operation (0-exit, 1-encrypt, 2-decrypt): ";
+        cout<<"Select operation (0-exit, 1-encrypt, 2-decrypt): "; //выбор операции
             cin >> op;
              if (op != 0) {
-            cout << "Cipher ready. Select an algorithm (0-exit, 1-AES, 2-DES): ";
+            cout << "Cipher ready. Select an algorithm (0-exit, 1-AES, 2-DES): ";         //выводит выбор алгоритма
         cin >> alg;
         }
        
-        if (op > 2) {
+        if (op > 2) {                      //ошибка в случае если выбран некорректная операция с порядковым номером больше двух
             cerr << "ERROR! Invalid operation" << endl;
         }
         else if (op > 0) {
-            cout << "Enter a path to input file: ";
+            cout << "Enter a path to input file: "; 
+            //тут типа выбор файла
             cin >> file_in;
             cout << "Enter a path to output file: ";
             cin >> file_out;
-            cout << "Enter a password: ";
-            cin >> key;
+            cout << "Enter a password: "; 
+            cin >> key;                                 //вводим ключ
             if (alg == 1) {
-                AES_CRYP aes(file_in, file_out, key);
+                AES_CRYP aes(file_in, file_out, key); //алгоритм если выбран метод AES
                 if (op == 1) {
                     if (aes.encrypt())
-                        cout << "Encription completed!" << endl;
-                    else
-                        cout << "Encryption failed!" << endl;
+                        cout << "Encription completed!" << endl; //если шифрование работает
+                    else                                              
+                        cout << "Encryption failed!" << endl; //если не работает
                 } else {
                     if (aes.decrypt())
-                        cout << "Decription completed!" << endl;
+                        cout << "Decription completed!" << endl; //если расшифровало 
                     else
-                        cout << "Decryption failed!" << endl;
+                        cout << "Decryption failed!" << endl; //идешь на пересдачу
                 }
             }
-            else if (alg == 2) {
+            else if (alg == 2) { //если выбран алгоритм DES
                 DES_CRYP des(file_in, file_out, key);
                 if (op == 1) {
                     if (des.encrypt())
