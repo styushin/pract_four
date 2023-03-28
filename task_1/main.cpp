0742c94a1adc33fdaf7237b3a7ca7a148cee875f
styushin <84208644+styushin@users.noreply.github.com>
2023-03-28 23:58:15 +0300

    Update main.cpp

--- task_1/main.cpp
+++ task_1/main.cpp
@@ -6,31 +6,32 @@
 #include <cryptopp/hex.h>
 #include <cryptopp/files.h>
 #include <cryptopp/sha.h>
+//подключение библиотек
 using namespace std;
 int main ()
 {
     CryptoPP::SHA1 hash;
 
-    cout << "Имя: " << hash.AlgorithmName() << endl;
+    cout << "Имя: " << hash.AlgorithmName() << endl; //ввод
     cout << "Размер хэша: " << hash.DigestSize() << endl;
     cout << "Размер блока: " << hash.BlockSize() << endl;
 
-    string soo = "";
+    string soo = ""; //переменная , куда кладется все из файла
     string stroka;
-    ifstream f("/home/student/soo.txt");
+    ifstream f("/home/student/soo.txt"); //считывает файл в бинарном виде
     while (getline(f, stroka)) {
-        soo += stroka;
+        soo += stroka;     //кладет в переменную
     }
 
 
-    cout << "Сообщение: " << soo << endl;
-    vector<byte> digest(hash.DigestSize());
+    cout << "Сообщение: " << soo << endl; //вывод сообщения
+    vector<byte> digest(hash.DigestSize()); //
 
-    hash.Update((const byte*)soo.data(), soo.size());
+    hash.Update((const byte*)soo.data(), soo.size()); 
     hash.Final(digest.data());
 
     cout << "Хэш: ";
-    CryptoPP::StringSource(digest.data(), digest.size(), true, new CryptoPP::HexEncoder(new CryptoPP::FileSink(cout)));
+    CryptoPP::StringSource(digest.data(), digest.size(), true, new CryptoPP::HexEncoder(new CryptoPP::FileSink(cout))); //приводит в адекватный вид
     cout << endl;
     return 0;
 }
